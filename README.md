8a103ddf47af6f04ff268bd401256990b4c523f2
Vincent Ning <vincent.ning93@gmail.com>
2017-01-25 14:21:58 -0800

    Exponent Starter Kit

---
+++ README.md
@@ -0,0 +1,18 @@
+# hello-graphql
+
+- An example project that integrates Exponent with [Apollo
+Client](https://github.com/apollostack/apollo-client) to interact with a
+[Scaphold](https://scaphold.io/) backend.
+
+- Try it here: https://getexponent.com/@community/hello-graphql
+
+
+## Running it locally
+
+Clone this repo and `npm install` and open it with XDE or exp.
+
+If you want to use your own Scaphold account, first sign up on
+[Scaphold.io](https://scaphold.io/), then create a database.
+Next, swap out the `networkInterface` `uri` in `main.js` with
+the uri of your fancy new database ([find your uri on the Scaphold
+apps page](https://scaphold.io/apps)).
---
+++ main.js
@@ -0,0 +1,163 @@
+import Exponent from 'exponent';
+import React from 'react';
+import {
+  ActivityIndicator,
+  Dimensions,
+  KeyboardAvoidingView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import { ApolloProvider, graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+
+const client = new ApolloClient({
+  networkInterface: createNetworkInterface({
+    uri: 'https://us-west-2.api.scaphold.io/graphql/exponent-starter-kit'
+  }),
+});
+
+class AppContainer extends React.Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    );
+  }
+}
+
+@graphql(gql`
+  query MessagesQuery {
+    viewer {
+      allMessages(last: 25) {
+        edges {
+          node {
+            text
+          }
+        }
+      }
+    }
+  }
+`)
+class App extends React.Component {
+  render() {
+    if (this.props.data.loading) {
+      return (
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size="large" />
+        </View>
+      );
+    }
+
+    let messages = this.props.data.viewer.allMessages.edges;
+
+    return (
+      <View style={styles.container}>
+        <ScrollView
+          keyboardDismissMode="on-drag"
+          style={{flex: 1}}
+          contentContainerStyle={{paddingTop: 30, paddingBottom: 50, paddingHorizontal: 10}}>
+          {messages.map((message, i) => <Text key={i}>{message.node.text}</Text>)}
+        </ScrollView>
+
+
+        {
+          /**
+            * Note: KeyboardAvoidingView won't quite work as expected after fresh npm install
+            * b/c it is missing this commit:
+            * https://github.com/facebook/react-native/commit/ec6e2741720d70f7523477bf4d4e421cac9a19e1
+            **/
+        }
+        <KeyboardAvoidingView
+          behavior="padding"
+          style={{position: 'absolute', bottom: 0, left: 0, right: 0}}>
+          <MessageInput onNewMessage={() => this.props.data.refetch()} />
+        </KeyboardAvoidingView>
+
+        <View style={styles.statusBarUnderlay} />
+        <StatusBar barStyle="light-content" />
+      </View>
+    );
+  }
+}
+
+
+@graphql(gql`
+  mutation AddMessage($msg: CreateMessageInput!) {
+    createMessage(input: $msg) {
+      changedMessage {
+        text
+      }
+    }
+  }
+`)
+class MessageInput extends React.Component {
+  state = {
+    text: '',
+  }
+
+  _handleSubmission = async () => {
+    try {
+      let result = await this.props.mutate({
+        variables: {
+          msg: {
+            text: this.state.text
+          }
+        }
+      });
+      this.setState({text: ''});
+      this.props.onNewMessage();
+    } catch(e) {
+      alert(e.message);
+    }
+  }
+
+  render() {
+    return (
+      <TextInput
+        onChangeText={text => this.setState({text})}
+        onSubmitEditing={this._handleSubmission}
+        placeholder="Leave a message"
+        style={styles.messageInput}
+        value={this.state.text}
+        returnKeyType="send"
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  messageInput: {
+    backgroundColor: '#fff',
+    height: 50,
+    width: Dimensions.get('window').width,
+    borderWidth: 1,
+    borderColor: '#eee',
+    paddingHorizontal: 10,
+  },
+  statusBarUnderlay: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    left: 0,
+    height: Exponent.Constants.statusBarHeight,
+    backgroundColor: '#888',
+  },
+});
+
+Exponent.registerRootComponent(AppContainer);
